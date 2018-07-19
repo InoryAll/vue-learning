94c34a3db523158abf9d567938a59817083fabb3
Jie Ren Tian <110873204@qq.com>
2018-07-19 17:40:54 +0800

    vuex的接入.

---
+++ test/src/store/index.js
@@ -0,0 +1,13 @@
+/**
+ * 在vue中使用vuex
+ * Created by tianrenjie on 2018/7/19
+ */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default Vuex.Store({
+  modules: {
+  },
+});
