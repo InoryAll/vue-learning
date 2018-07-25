125575aa828cfbbb9b7ddcea54c1fd2022244a80
Jie Ren Tian <110873204@qq.com>
2018-07-25 17:48:51 +0800

    筛选器动画效果添加.

--- test/src/components/filterUtil.vue
+++ test/src/components/filterUtil.vue
@@ -2,29 +2,48 @@
   <div class="filter-util">
     <flexbox>
       <flexbox-item>
-        <button class="sort-btn filter-util-btn">
+        <button class="sort-btn filter-util-btn" @click="handleAnimationClick('sortActive')">
           <span>排序</span>
-          <x-icon class="filter-util-icon" type="ios-arrow-down" size="18"></x-icon>
+          <x-icon
+            class="filter-util-icon"
+            :class="{ rotateIn: sortActive === 1, rotateOut: sortActive === -1 }"
+            type="ios-arrow-down"
+            size="18"></x-icon>
         </button>
       </flexbox-item>
       <flexbox-item>
-        <button class="insurance-btn filter-util-btn">
+        <button
+          class="insurance-btn filter-util-btn"
+          @click="handleAnimationClick('insuranceActive')">
           <span>特色保障</span>
-          <x-icon class="filter-util-icon" type="ios-arrow-down" size="18"></x-icon>
+          <x-icon
+            class="filter-util-icon"
+            :class="{ rotateIn: insuranceActive === 1, rotateOut: insuranceActive === -1 }"
+            type="ios-arrow-down"
+            size="18"></x-icon>
         </button>
       </flexbox-item>
       <flexbox-item>
-        <button class="filter-btn filter-util-btn">
+        <button class="filter-btn filter-util-btn" @click="handleAnimationClick('filterActive')">
           <span>筛选</span>
-          <x-icon class="filter-util-icon" type="ios-arrow-down" size="18"></x-icon>
+          <x-icon
+            class="filter-util-icon"
+            :class="{ rotateIn: filterActive === 1, rotateOut: filterActive === -1 }"
+            type="ios-arrow-down"
+            size="18"></x-icon>
         </button>
       </flexbox-item>
     </flexbox>
+    <div class="sort-pop-up"></div>
+    <div class="insurance-pop-up"></div>
+    <div class="filter-pop-up"></div>
   </div>
 </template>
 
 <script>
 import { Flexbox, FlexboxItem } from 'vux';
+// eslint-disable-next-line
+import $ from 'jquery';
 
 export default {
   name: 'filter-util',
@@ -33,14 +52,36 @@ export default {
     FlexboxItem,
   },
   data() {
-    return {};
+    return {
+      sortActive: 0,
+      insuranceActive: 0,
+      filterActive: 0,
+    };
   },
   computed: {},
-  methods: {},
+  methods: {
+    handleAnimationClick(paramName) {
+      if (this[paramName] === 0) {
+        this[paramName] = 1;
+        return;
+      }
+      this[paramName] = -1 * this[paramName];
+    },
+  },
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+  @import "../animation.less";
+
+  .rotateIn{
+    animation: rotateIn 0.3s linear 0s 1;
+    animation-fill-mode: both;
+  }
+  .rotateOut{
+    animation: rotateOut 0.3s linear 0s 1;
+    animation-fill-mode: both;
+  }
   .filter-util{
     height: 40px;
     border-bottom: 1px solid #d9d9d9;
