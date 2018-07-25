2c53956445af9b0c734cbe8da1e6faae3c435458
Jie Ren Tian <110873204@qq.com>
2018-07-25 09:53:03 +0800

    添加vux依赖，一个vue的组件库.

--- test/src/main.js
+++ test/src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import vueScrollBehavior from 'vue-scroll-behavior';
 import App from './App';
 import router from './router';
 import store from './store';
-import vueScrollBehavior from 'vue-scroll-behavior';
+
 
 Vue.config.productionTip = false;
 
-Vue.use(vueScrollBehavior, { router: router });
+// 转到一个新页面时，定位到最顶端
+Vue.use(vueScrollBehavior, { router });
 
 /* eslint-disable no-new */
 new Vue({
